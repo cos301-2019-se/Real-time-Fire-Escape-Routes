994d9472734046cc77a96a481b154692aedc1cbe
JasonLouw <mjasonlouw@gmail.com>
2019-05-01 22:50:38 +0200

    adding unity and server together

---
+++ MapUnity/Assets/scripts/AgentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+         //agent.SetDestination(GameObject.FindWithTag("door").transform.position);
+        // agent.destination = new Vector3(1,1,1);
+    }
+
+    public void goTo(int x)
+    {
+
+
+
+        var goArray = FindObjectsOfType<GameObject>();
+       
+            for (int j = 0; j < goArray.Length; j++)
+            {
+            if (goArray[j].GetComponent<number>() != null)
+                if (goArray[j].GetComponent<number>().objectNumber == x)
+                {
+                    agent.SetDestination( goArray[j].transform.position);
+                }
+            }
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
