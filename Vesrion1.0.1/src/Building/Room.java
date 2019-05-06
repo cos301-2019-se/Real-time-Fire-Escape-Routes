25f23af65a5e88f57dba5b60eb39ce05cac76c42
Pieter <u16009917@tuks.co.za>
2019-05-06 19:25:59 +0200

    Made Room building easier

--- Vesrion1.0.1/src/Building/Room.java
+++ Vesrion1.0.1/src/Building/Room.java
@@ -15,16 +15,6 @@ public class Room {
     public Vector<Door> doors = new Vector<Door>();
     private Vector<Person> peopleInRoom = new Vector<>();
 
-    public Vector<Room> getRooms() {
-        return Rooms;
-    }
-    public Room getRooms(int i ) {
-        return Rooms.get(i);
-    }
-
-    public void addRoom(Room room) {
-        Rooms.add(room);
-    }
 
     private Vector<Room> Rooms = new Vector<Room>();
     private Vector<Corner> Corners = new Vector<>();
@@ -35,22 +25,78 @@ public class Room {
         roomType = type;
         Walls.add(new Vector<>());
     }
+    /**Self Explanatory*/
+    public Vector<Room> getRooms() {
+        return Rooms;
+    }
+    /**Self Explanatory*/
+    public Room getRooms(int i ) {
+        return Rooms.get(i);
+    }
+    /**Self Explanatory*/
+    public void addRoom(Room room) {
+        Rooms.add(room);
+    }
+
+    /**
+     * @Description: Returns a string specifying the type of room and if it is valid
+     * */
     public String isValidRoom(){
         if(!isCyclic())
             return roomType.name()+ " (room) has an error";
         return roomType.name()+ " (room) is complete";
     }
-    void addCorner(Corner c){
+    /**
+     * @Description: Adds a corner to the room, Take Note it means nothing on its own
+     * */
+    protected void addCorner(Corner c){
         Corners.add(c);
         Walls.add(new Vector<>());
     }
+
     protected Corner getCorner(int i){
         return Corners.get(i);
     }
+
+    /**
+     * Connects 2 corners and adds them into the wall array
+     * You can call this function with corners not previously added since it will add it automatically
+     * */
     void buildWall(Corner v, Corner w) {
+        int i= Corners.indexOf(v);      //  This part prevents
+        if(i== -1)                      //  errors by making sure
+            addCorner(v);               //  the corner exists in
+        int j = Corners.indexOf(w);     //  the room
+        if(j== -1)                      //
+            addCorner(w);               //
+
         Walls.get(Corners.indexOf(v)).add(w);
         Walls.get(Corners.indexOf(w)).add(v);
     }
+    /**
+     * Just a simple way of building Walls
+     * */
+    void buildWall(double [] corner1, double[] corner2 ) {
+
+        Corner a = null;
+        Corner b = null;
+        for (int i = 0; i < Corners.size(); i++) {
+            double [] current =Corners.get(i).getCoords();
+            if(current[0] == corner1[0] && current[1] == corner1[1])
+                a= Corners.get(i);
+            if(current[0] == corner2[0] && current[1] == corner2[1])
+                b= Corners.get(i);
+        }
+        if(a==null)
+            a = new Corner(corner1[0],corner1[1]);
+        if(b==null)
+            b = new Corner(corner2[0],corner2[1]);
+        buildWall(a,b);
+    }
+
+    /**
+     * @info : See the function "isCyclic()" for more details
+     * */
     Boolean isCyclicUtil(Corner v, Boolean visited[], Corner parent) {
         int _v = Corners.indexOf(v);
         visited[_v] = true;
@@ -68,6 +114,11 @@ public class Room {
         }
         return false;
     }
+    /**
+     * @return :
+     *  true - Returns true if it contains a cycle
+     *  false - Returns false if it still needs walls
+     * */
     Boolean isCyclic() {
         Boolean visited[] = new Boolean[Corners.size()];
         for (int i = 0; i < Corners.size(); i++)
@@ -78,8 +129,6 @@ public class Room {
                     return true;
         return false;
     }
-
-
 
     public static class Corner{
         double x;
--- Vesrion1.0.1/src/Building/RoomTest.java
+++ Vesrion1.0.1/src/Building/RoomTest.java
@@ -6,11 +6,21 @@ import static Building.RoomType.hall;
 
 public class RoomTest {
 
+    private static double [][] CorridoorCorners = {
+            {20,0}, //First corner
+            {25,0},
+            {25,20},
+            {50,20},
+            {50,25},
+            {20,25}, // Last corner
+    };
+
     public static void main(String[] args)
     {
         Building Demo = new Building();
 
         // Create a graph given in the above diagram
+        /*
         Room r1 = new Room(floor);
         Room.Corner c1 = new Room.Corner(0,0);
         Room.Corner c2 = new Room.Corner(0,50);
@@ -32,25 +42,21 @@ public class RoomTest {
             Demo.addFloor(r1);
         else
             return;
+
         Room Groundfloor = Demo.getFloor(0);
+    */
         Room corridoor = new Room(hall);
-        corridoor.addCorner(new Room.Corner(20,0));
-        corridoor.addCorner(new Room.Corner(25,0));
-        corridoor.addCorner(new Room.Corner(25,20));
-        corridoor.addCorner(new Room.Corner(50,20));
-        corridoor.addCorner(new Room.Corner(50,25));
-        corridoor.addCorner(new Room.Corner(20,25));
-        corridoor.buildWall(corridoor.getCorner(0),corridoor.getCorner(1));
-        corridoor.buildWall(corridoor.getCorner(1),corridoor.getCorner(2));
-        corridoor.buildWall(corridoor.getCorner(2),corridoor.getCorner(3));
-        corridoor.buildWall(corridoor.getCorner(3),corridoor.getCorner(4));
-        corridoor.buildWall(corridoor.getCorner(4),corridoor.getCorner(5));
-        corridoor.buildWall(corridoor.getCorner(5),corridoor.getCorner(0));
+        for (int i = 0; i < CorridoorCorners.length-1; i++) {
+            corridoor.buildWall(CorridoorCorners[i],CorridoorCorners[i+1]);
+        }
+        corridoor.buildWall(CorridoorCorners[CorridoorCorners.length-1],CorridoorCorners[0]);
         System.out.println(corridoor.isValidRoom());
+        /*
         if(corridoor.isCyclic())
             Groundfloor.addRoom(corridoor);
         else
             return;
+            */
     }
 
 }
