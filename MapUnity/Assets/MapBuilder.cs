785a20aef97c040af0d844f2fc750f1e0b08a12a
JasonLouw <mjasonlouw@gmail.com>
2019-05-07 20:22:16 +0200

    dynamic map creator

--- MapUnity/Assets/MapBuilder.cs
+++ MapUnity/Assets/MapBuilder.cs
@@ -1,36 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MapBuilder : MonoBehaviour
 {
-    public GameObject square;
+    public NavMeshSurface s;
+    //public GameObject square;
     // Start is called before the first frame update
-    int i, j;
+    //int i, j;
+   
 
-    void Start()
+    void Awake()
     {
-        i = 0;
-        j = 0;
-        // for(int i = 0; i < 1000; i ++)
-        // {
-        //     for(int j = 0; j < 1000; j++ )
-        //     {
-        //         Instantiate(square, new Vector3(i,1,j), new Quaternion(0, 0 , 0, 1));
-        //     }
-        // }
+       // surface = GetComponent<NavMeshAgent> (); 
+        buildFloorTop();
+        //buildFloorBottom();
+
+
+        s.BuildNavMesh();
+
+   
         
+    }
+
+    void buildFloorTop()
+    {
+        Vector2[] vertices2D = new Vector2[] {
+            new Vector2(0,0),
+            new Vector2(0,50),
+            new Vector2(25,50),
+        };
+ 
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+ 
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i=0; i<vertices.Length; i++) {
+            vertices[i] = new Vector3(vertices2D[i].x, 0 , vertices2D[i].y);
+        }
+ 
+        // Create the mesh
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+        // msh.RecalculateNormals();
+        // msh.RecalculateBounds();
+ 
+        // Set up game object with mesh;
+        gameObject.AddComponent(typeof(MeshRenderer));
+        MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        filter.mesh = msh;
+    }
+
+     void buildFloorBottom()
+    {
+        Vector2[] vertices2D = new Vector2[] {
+            new Vector2(0,0),
+            new Vector2(0,50),
+            new Vector2(25,50),
+        };
+ 
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+ 
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i=0; i<vertices.Length; i++) {
+            vertices[i] = new Vector3(vertices2D[i].x, 0 , vertices2D[i].y);
+        }
+ 
+        // Create the mesh
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+        // msh.RecalculateNormals();
+        // msh.RecalculateBounds();
+ 
+        // Set up game object with mesh;
+        gameObject.AddComponent(typeof(MeshRenderer));
+        MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        filter.mesh = msh;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(i < 100)
-            Instantiate(square, new Vector3(i++,1,j), new Quaternion(0, 0 , 0, 1));
-        else
-        {
-            j++;
-            i = 0;
-        }
+        // if(i < 100)
+        //     Instantiate(square, new Vector3(i++,1,j), new Quaternion(0, 0 , 0, 1));
+        // else
+        // {
+        //     j++;
+        //     i = 0;
+        // }
     }
 }
