16540d79db862eeb1780624912c210dce638751a
Pieter <u16009917@tuks.co.za>
2019-05-24 09:44:05 +0200

    Merge resolved

--- src/test/java/HTTPServerTest.java
+++ src/test/java/HTTPServerTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Vesrion1.0.1/src/HTTPServerTest.java
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,21 +13,4 @@ public class HTTPServerTest {
         thread.stop();
         System.out.println("HTTPServer runs correctly --  passed");
     }
-=======
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class HTTPServerTest {
-    @Test
-    public void testOne()
-    {
-
-        Thread thread = new Thread(new HTTPServer(null));
-        thread.start();
-
-        thread.stop();
-        System.out.println("HTTPServer runs correctly --  passed");
-    }
->>>>>>> TravisTesting:src/test/java/HTTPServerTest.java
 }
