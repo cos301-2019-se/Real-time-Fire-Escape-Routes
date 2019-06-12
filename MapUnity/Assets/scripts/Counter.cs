4da0156ada9f14b7b75912d22f5990338437f07f
JasonLouw <mjasonlouw@gmail.com>
2019-06-12 13:05:23 +0200

    changed ip

--- MapUnity/Assets/scripts/Counter.cs
+++ MapUnity/Assets/scripts/Counter.cs
@@ -69,8 +69,8 @@ public class Counter : MonoBehaviour
                 buildingOb.GetComponent<Building>().addStrings(myObject);
                 buildingOb.GetComponent<Building>().createArrays();
                 s.BuildNavMesh();
-                //StartCoroutine(postRequest("http://localhost:8080/building", "{\"type\":\"assignPeople\"}","assignPeople"));
-                StartCoroutine(postRequest("http://192.168.1.39:8080/building", "{\"type\":\"assignPeople\"}", "assignPeople"));
+                StartCoroutine(postRequest("http://192.168.43.237:8080/building", "{\"type\":\"assignPeople\"}","assignPeople"));
+               // StartCoroutine(postRequest("http://192.168.1.39:8080/building", "{\"type\":\"assignPeople\"}", "assignPeople"));
             }
             else if(type == "assignPeople")
             {
@@ -91,6 +91,7 @@ public class Counter : MonoBehaviour
                     string[] x1 = x[i].Split('*');
                     values[i][0] = float.Parse(x1[0], System.Globalization.CultureInfo.InvariantCulture);
                     string[] x2 = x1[1].Split(',');
+                   
                     values[i][1] = float.Parse(x2[0], System.Globalization.CultureInfo.InvariantCulture);
                     values[i][2] = float.Parse(x2[1], System.Globalization.CultureInfo.InvariantCulture);
                 }
@@ -274,6 +275,6 @@ public class Counter : MonoBehaviour
        //StartCoroutine(postRequest("http://10.5.50.202:8080/", json));//kinson
 
 
-        StartCoroutine(postRequest("http://192.168.1.39:8080/buildingGeneration", "{\"type\":\"buildingData\"}","buildingGeneration"));//kinson
+        StartCoroutine(postRequest("http://192.168.43.237:8080/buildingGeneration", "{\"type\":\"buildingData\"}","buildingGeneration"));//kinson
     }
 }
