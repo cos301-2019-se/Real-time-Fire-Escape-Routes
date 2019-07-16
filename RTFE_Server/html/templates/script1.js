96fab996c6061c9f9b7f59ce2b0bd6feb2b6a18e
Kateryna Reva <u17035989@tuks.co.za>
2019-07-16 20:41:46 +0200

    navigation was changed

--- RTFE_Server/html/templates/script1.js
+++ RTFE_Server/html/templates/script1.js
@@ -7,7 +7,7 @@ $(()=>{
 
 	let rowForBar = $("#top-bar");
 	//fetchFromDb("getUsers");
-	rowForBar.append(echoTopBar);
+	rowForBar.append(echoTopBar2);
 	let main1 =  $(".main");
 	main1.append(`<div class="main-cards">`);
 	let main = $(".main-cards");
@@ -16,6 +16,20 @@ $(()=>{
 	main.append(echoSimulationWindow());
 	main.append(echoBotCreator());
 	main.append(echoFireEditor());
+
+	$("#toggler").on("click", ()=>{
+		console.log($("#toggler").attr("aria-expanded"));
+
+		if($("#toggler").attr("aria-expanded") === "true")
+		{
+			$("#toggler").attr("style", "margin-top: 2px;");
+		}
+		else
+		{
+			$("#toggler").attr("style", "margin-top: 100px;");
+		}
+
+	})
 
 	/*$("form div div button").on("click", (e)=>{
 		e.preventDefault();
@@ -81,6 +95,27 @@ function echoTopBar()
 						    <li id="admin-view" class="nav__list-item">Admin view</li>
 					  	</ul>
   			<div class="header__avatar"><img id="icons" src="icons/white_person.png"> <span>User Name</span></div>
+  					`;
+
+}
+
+function echoTopBar2()
+{
+	return `<nav class="navbar navbar-expand-lg navbar-light" id="nav2">
+				  <a class="navbar-brand header__name" href="#"><img class="" style="width: 220px;" src="img/fireG.png">
+	  					</a>
+				  <button class="navbar-toggler" style="" id="toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				    <span class="navbar-toggler-icon"></span>
+				  </button>
+
+				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+				    <ul class="navbar-nav mr-auto nav__list" id="admin-super-choice">
+						    <li  id="super-user-view" class="active nav__list-item">Super-user view</li>
+						    <li id="admin-view" class="nav__list-item">Admin view</li>
+					  	</ul>
+				     
+				  </div>
+				</nav>
   					`;
 
 }
