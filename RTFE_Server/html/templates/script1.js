60f96bacf0fbb369835eac22dc320b96285d1552
Pieter <u16009917@tuks.co.za>
2019-07-18 08:52:28 +0200

    PreDemoChanges
    - Updated travis
    - Minor html changes

--- RTFE_Server/html/templates/script1.js
+++ RTFE_Server/html/templates/script1.js
@@ -848,6 +848,7 @@ function getInfoFromInput(callFunc, email1)
 		addFire(x, y, rad, floor);
 	}
 
+
 		clearWindow();
 }
 
@@ -1052,6 +1053,19 @@ function addFire(x, y, rad, floor)
             success: function(data){
                 if (data.status){
                 	notify(data.message, "blue");
+                		$.ajax({
+				            url: "http://127.0.0.1:8080/building",
+				            type: "POST",
+				            contentType: "application/json; charset=utf-8",
+				            dataType: "json",
+				            data: JSON.stringify({
+				                type: "assignPeople"
+				            }),
+				            success: function(data){
+				            	$("body").delay(1500);
+				                notify("Affected people were notified","green")
+				            }
+				        });
                     
                 }else{
                 	
