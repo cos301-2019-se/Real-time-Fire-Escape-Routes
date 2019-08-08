a004b2d3b3c97562a25877a8ebc635f9ffa7ad3d
Kateryna Reva <u17035989@tuks.co.za>
2019-08-08 10:14:17 +0200

    :lipstick:(Design):fixed overflow problem

--- RTFE_Server/html/js/script1.js
+++ RTFE_Server/html/js/script1.js
@@ -103,11 +103,9 @@ function echoTopBar()
 function echoTopBar2()
 {
 	return `<nav class="navbar navbar-expand-lg navbar-light" id="nav2">
-				  <a class="navbar-brand header__name" href="#"><img class="" style="width: 220px; padding-top: 8px;" src="img/fireG.png">
+				  <!--<a class="navbar-brand header__name" href="#"><img class="" style="width: 220px; padding-top: 8px;" src="img/fireG.png">
 	  					</a>
-				 <!--<button class="navbar-toggler" style="" id="toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				    <span class="navbar-toggler-icon"></span>
-				  </button>-->
+				 
 
 				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 				    <ul class="navbar-nav mr-auto nav__list" id="admin-super-choice">
@@ -115,7 +113,7 @@ function echoTopBar2()
 						    <li id="admin-view" class="nav__list-item">Admin view</li>
 					  	</ul>
 				     
-				  </div>
+				  </div>-->
 				</nav>
   					`;
 
